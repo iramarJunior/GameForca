017819a048de188fdf8c2fcf49549393743598a1
iramarJunior <iramar.j@hotmail.com>
2019-02-17 14:56:09 -0300

    Continuando o Game.

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -13,13 +13,22 @@
 (defn acertou-a-palavra-toda? [palavra acertos] 
   (empty? letras-faltantes palavra acertos))
 
+(defn le-letra! [] (read-line))
+
+(defn acertou? [chute palavra] (.contains palavra chute))
+
+(defn avalia-chute [chute vidas palavra acertos]
+  (if (acertou? chute palavra)
+    (jogo vidas palavra (conj acertos chute))
+    (jogo (dec vidas) palavra acertos)))
+
 (defn jogo [vidas palavra acertos]
   (if (= vidas 0)
     (perdeu)
     (if (acertou-a-palavra-toda? palavra acertos)
       (ganhou)
+      (avalia-chute (le-letra!) vidas palavra acertos)
       (print "Chuta amigo."))))
 
-(defn -main
-  [& args]
-  (jogo total-de-vidas "Melancia" #{"L" "M"}))
+(defn -main [& args]
+  (jogo total-de-vidas)
